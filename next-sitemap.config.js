cf3fbe8baa5000eb0fd6b6fcd94681aa2469e07a
PierreDillard <110824159+PierreDillard@users.noreply.github.com>
2025-01-09 22:14:00 +0100

    Update next-sitemap.config

---
+++ next-sitemap.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  siteUrl: 'https://www.ensemblepourleroyaume.com',
+  generateRobotsTxt: true, // GÃ©nÃ¨re Ã©galement un fichier robots.txt
+  sitemapSize: 5000, // Taille maximale pour le sitemap
+  changefreq: 'daily', // FrÃ©quence de mise Ã  jour
+  priority: 0.7, // PrioritÃ© par dÃ©faut
+};
