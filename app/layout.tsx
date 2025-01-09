a376edd6270a445930e3c48c4baa60073878db7d
PierreDillard <7gaspard77@gmail.com>
2025-01-09 17:12:20 +0100

    favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -64,10 +64,19 @@ export default function RootLayout({
     <html lang="fr" suppressHydrationWarning >
          <ScrollHandler /> 
          <head>
-  <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
-  <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-  <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
-  <link rel="icon" type="image/x-icon" href="/favicon.ico" />
+         <link rel="icon" href="/favicon.ico" sizes="any" />
+         <link
+  rel="apple-touch-icon"
+  href="/apple-icon?<generated>"
+  type="image/<generated>"
+  sizes="<generated>"
+/>
+  <link
+  rel="icon"
+  href="/icon?<generated>"
+  type="image/<generated>"
+  sizes="<generated>"
+/>
   <link rel="manifest" href="/site.webmanifest" />
   <meta name="msapplication-TileColor" content="#ffffff" />
   <meta name="theme-color" content="#ffffff" />
