97a981309aa7b8a2a62f884ef8c2cd9ccc88d351
PierreDillard <7gaspard77@gmail.com>
2025-01-23 09:30:22 +0100

    fix contact subtitle

--- components/sections/contact/index.tsx
+++ components/sections/contact/index.tsx
@@ -9,7 +9,7 @@ export default function Contact() {
   return (
     <section id="contact" className="py-8 bg-gray-50">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-        <div className="text-center mb-8">
+        <div className="r mb-8">
 
            <SectionTitle 
           title="Contact"
